98d6ed73e15088b946328f88de04a4c162fd4c34
KuhnLuisMatias <matiaskuhn.93@gmail.com>
2023-10-24 23:35:07 -0300

    Foodie ASP.NET

---
+++ Foodie/User/User.Master.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Foodie.User
+{
+    public partial class User : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Request.Url.AbsoluteUri.ToString().Contains("Default.aspx"))
+                form1.Attributes.Add("class", "sub_page");
+            else
+            {
+                Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl.ascx");
+                pnlSliderUC.Controls.Add(sliderUserControl);
+            }
+        }
+    }
+}
