23a480c94f0a7b697a5d6153db5cee7f116c7ed1
KuhnLuisMatias <matiaskuhn.93@gmail.com>
2023-11-18 15:23:12 -0300

    Update Status

--- Foodie/User/Profile.aspx.cs
+++ Foodie/User/Profile.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+using System.Web.UI;
 
 namespace Foodie.User
 {
@@ -17,7 +19,10 @@ namespace Foodie.User
                 if (Session["userId"] == null)
                     Response.Redirect("Login.aspx");
                 else
+                {
                     GetUserDetails();
+                    GetPurchaseHistory();
+                }
             }
         }
         void GetUserDetails()
@@ -39,6 +44,82 @@ namespace Foodie.User
                 Session["email"] = dt.Rows[0]["Email"].ToString();
                 Session["imageUrl"] = dt.Rows[0]["ImageUrl"].ToString();
                 Session["createdDate"] = dt.Rows[0]["CreatedDate"].ToString();
+            }
+        }
+        void GetPurchaseHistory()
+        {
+            int sr = 0;
+            con = new SqlConnection(Connection.GetConnectionString());
+            cmd = new SqlCommand("Invoice", con);
+            cmd.Parameters.AddWithValue("@Action", "ODRHISTORY");
+            cmd.Parameters.AddWithValue("@UserId", Session["userId"]);
+            cmd.CommandType = CommandType.StoredProcedure;
+            sda = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            sda.Fill(dt);
+            dt.Columns.Add("SrNo", typeof(Int32));
+            if (dt.Rows.Count > 0)
+            {
+                foreach (DataRow dataRow in dt.Rows)
+                {
+                    dataRow["SrNo"] = sr;
+                    sr++;
+                }
+            }
+
+            if (dt.Rows.Count == 0)
+            {
+                rPurchaseHistory.FooterTemplate = null;
+                rPurchaseHistory.FooterTemplate = new CustomTemplate(ListItemType.Footer);
+            }
+            rPurchaseHistory.DataSource = dt;
+            rPurchaseHistory.DataBind();
+        }
+        protected void rPurchaseHistory_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                double grandTotal = 0;
+                HiddenField paymentId = e.Item.FindControl("hdnPaymentId") as HiddenField;
+                Repeater repOrders = e.Item.FindControl("rOrders") as Repeater;
+
+                con = new SqlConnection(Connection.GetConnectionString());
+                cmd = new SqlCommand("Invoice", con);
+                cmd.Parameters.AddWithValue("@Action", "INVOICBYID");
+                cmd.Parameters.AddWithValue("@PaymentId", Convert.ToInt32(paymentId.Value));
+                cmd.Parameters.AddWithValue("@UserId", Session["userId"]);
+                cmd.CommandType = CommandType.StoredProcedure;
+                sda = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                sda.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    foreach (DataRow dataRow in dt.Rows)
+                    {
+                        grandTotal += Convert.ToDouble(dataRow["TotalPrice"]);
+                    }
+                }
+                DataRow dr = dt.NewRow();
+                dr["TotalPrice"] = grandTotal;
+                repOrders.DataSource = dt;
+                repOrders.DataBind();
+            }
+        }
+
+        private sealed class CustomTemplate : ITemplate
+        {
+            private ListItemType ListItemType { get; set; }
+            public CustomTemplate(ListItemType type)
+            {
+                ListItemType = type;
+            }
+            public void InstantiateIn(Control container)
+            {
+                if (ListItemType == ListItemType.Footer)
+                {
+                    var footer = new LiteralControl("<tr><td colspan='5'><b>Your Cart is empty.</b><a href='Menu.aspx' class='badge badge-info ml-2'>Continue Shopping</a></td></tr></tbody></table>");
+                    container.Controls.Add(footer);
+                }
             }
         }
     }
